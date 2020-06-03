52d7c3901f0b80f5e81591e056ba9f2dc73f5019
Piotr Tokarski <piotr@profound.net>
2020-06-03 18:28:36 +0200

    Add movies model.

--- viewer/models.py
+++ viewer/models.py
@@ -1,5 +1,17 @@
-from django.db.models import CharField, Model
+from django.db.models import (
+    DO_NOTHING, CharField, DateField, DateTimeField, ForeignKey, IntegerField,
+    Model, TextField
+)
 
 
 class Genre(Model):
     name = CharField(max_length=128)
+
+
+class Movie(Model):
+    title = CharField(max_length=128)
+    genre = ForeignKey(Genre, on_delete=DO_NOTHING)
+    rating = IntegerField()
+    released = DateField()
+    description = TextField(null=True, blank=True)
+    created = DateTimeField(auto_now_add=True)
