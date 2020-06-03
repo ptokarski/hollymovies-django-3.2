c895f3be9b3e6210ed88a0d2e153f03c5ec689fc
Piotr Tokarski <piotr@profound.net>
2020-06-03 18:58:39 +0200

    Register models in admin site.

--- hollymovies/urls.py
+++ hollymovies/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from viewer.models import Genre, Movie
 from viewer.views import hello
+
+admin.site.register(Genre)
+admin.site.register(Movie)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
