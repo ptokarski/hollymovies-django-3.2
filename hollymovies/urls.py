d4a688acbc45d68c0cd57b4740a3f455bb9dfd2c
Piotr Tokarski <piotr@profound.net>
2020-06-03 14:45:33 +0200

    Add hello view.

--- hollymovies/urls.py
+++ hollymovies/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from viewer.views import hello
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello', hello)
 ]
