fa8b6df49e5006bf2bc3f7b44e4a325998999d85
Piotr Tokarski <piotr@profound.net>
2020-06-04 14:33:47 +0200

    Replace general view with TemplateView.

--- viewer/views.py
+++ viewer/views.py
@@ -1,12 +1,8 @@
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import TemplateView
 
 from viewer.models import Movie
 
 
-class MoviesView(View):
-    def get(self, request):
-        return render(
-            request, template_name='movies.html',
-            context={'movies': Movie.objects.all()}
-        )
+class MoviesView(TemplateView):
+    template_name = 'movies.html'
+    extra_context = {'movies': Movie.objects.all()}
