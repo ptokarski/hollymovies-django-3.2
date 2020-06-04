4e85e5dcff64d99fe0949ff02f8f84b85d80727d
Piotr Tokarski <piotr@profound.net>
2020-06-04 14:21:10 +0200

    Add basic cbv.

--- viewer/views.py
+++ viewer/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.views import View
 
 from viewer.models import Movie
 
 
-def movies(request):
-    return render(
-        request, template_name='movies.html',
-        context={'movies': Movie.objects.all()}
-    )
+class MoviesView(View):
+    def get(self, request):
+        return render(
+            request, template_name='movies.html',
+            context={'movies': Movie.objects.all()}
+        )
