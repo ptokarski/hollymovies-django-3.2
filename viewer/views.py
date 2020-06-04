5d96074fdd909b481548ee23d6fa9d8714be1ed9
Piotr Tokarski <piotr@profound.net>
2020-06-04 21:38:13 +0200

    Introduce CreateView.

--- viewer/views.py
+++ viewer/views.py
@@ -1,7 +1,7 @@
 from logging import getLogger
 
 from django.urls import reverse_lazy
-from django.views.generic import FormView, ListView
+from django.views.generic import CreateView, ListView
 
 from viewer.forms import MovieForm
 from viewer.models import Movie
@@ -9,23 +9,11 @@ from viewer.models import Movie
 LOGGER = getLogger()
 
 
-class MovieCreateView(FormView):
+class MovieCreateView(CreateView):
 
     template_name = 'form.html'
     form_class = MovieForm
     success_url = reverse_lazy('movie_create')
-
-    def form_valid(self, form):
-        result = super().form_valid(form)
-        cleaned_data = form.cleaned_data
-        Movie.objects.create(
-            title=cleaned_data['title'],
-            genre=cleaned_data['genre'],
-            rating=cleaned_data['rating'],
-            released=cleaned_data['released'],
-            description=cleaned_data['description']
-        )
-        return result
 
     def form_invalid(self, form):
         LOGGER.warning('User provided invalid data.')
