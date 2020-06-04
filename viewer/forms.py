51d5a4fa8463afc3e7feaa832468dd2d8b64f6ca
Piotr Tokarski <piotr@profound.net>
2020-06-04 20:45:34 +0200

    Add example cleanups and validation.

--- viewer/forms.py
+++ viewer/forms.py
@@ -1,3 +1,7 @@
+import re
+from datetime import date
+
+from django.core.exceptions import ValidationError
 from django.forms import (
     CharField, DateField, Form, IntegerField, ModelChoiceField, Textarea
 )
@@ -5,9 +9,43 @@ from django.forms import (
 from viewer.models import Genre
 
 
+def capitalized_validator(value):
+    if value[0].islower():
+        raise ValidationError('Value must be capitalized.')
+
+
+class PastMonthField(DateField):
+
+    def validate(self, value):
+        super().validate(value)
+        if value >= date.today():
+            raise ValidationError('Only past dates allowed here.')
+
+    def clean(self, value):
+        result = super().clean(value)
+        return date(year=result.year, month=result.month, day=1)
+
+
 class MovieForm(Form):
-    title = CharField(max_length=128)
+
+    title = CharField(validators=[capitalized_validator])
     genre = ModelChoiceField(queryset=Genre.objects)
     rating = IntegerField(min_value=1, max_value=10)
-    released = DateField()
+    released = PastMonthField()
     description = CharField(widget=Textarea, required=False)
+
+    def clean_description(self):
+        # Force each sentence of the description to be capitalized.
+        initial = self.cleaned_data['description']
+        sentences = re.sub(r'\s*\.\s*', '.', initial).split('.')
+        return '. '.join(sentence.capitalize() for sentence in sentences)
+
+    def clean(self):
+        result = super().clean()
+        if result['genre'].name == 'Comedy' and result['rating'] > 5:
+            self.add_error('genre', '')
+            self.add_error('rating', '')
+            raise ValidationError(
+                "Comedies aren't so good to be rated over 5."
+            )
+        return result
