227da01327a912901db174c5b0d1d321fcb3acc0
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-06-10 22:21:14 +0200

    Add custom templatetags.

---
+++ viewer/templatetags/movie_extras.py
@@ -0,0 +1,19 @@
+from django.template import Library
+from django.utils.html import escape
+from django.utils.safestring import SafeString
+
+register = Library()
+
+
+@register.simple_tag
+def movie_format(movie, short=False):
+    if short:
+        return f'{movie.title} ({movie.released.year})'
+    return f'{movie.title} ({movie.released.year}) - {movie.genre}'
+
+
+@register.filter
+def attr_as_p(obj, attrname=False):
+    label = escape(attrname.capitalize())
+    value = escape(getattr(obj, attrname))
+    return SafeString(f'<p><strong>{label}:</strong> {value}</p>')
