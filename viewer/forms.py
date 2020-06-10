16cf0cd094fd9c2f55472c05b1028a7e4abb5261
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-06-10 23:29:00 +0200

    Add new form layout using oldschool method.

--- viewer/forms.py
+++ viewer/forms.py
@@ -26,6 +26,11 @@ class PastMonthField(DateField):
 
 class MovieForm(ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Movie
         fields = '__all__'
