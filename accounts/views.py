b44569662fe68407fee8122b2fbc2b43cfbbf3ab
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-06-14 16:27:48 +0200

    Add logout and password change.

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,17 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.urls import reverse_lazy
 
-from accounts.forms import SubmittableAuthenticationForm
+from accounts.forms import (
+    SubmittableAuthenticationForm, SubmittablePasswordChangeForm
+)
 
 
 class SubmittableLoginView(LoginView):
     form_class = SubmittableAuthenticationForm
     template_name = 'form.html'
+
+
+class SubmittablePasswordChangeView(PasswordChangeView):
+    form_class = SubmittablePasswordChangeForm
+    template_name = 'form.html'
+    success_url = reverse_lazy('index')
