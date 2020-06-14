d35ff34e8724c1010632df19992a33ed933efd8b
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-06-14 21:37:25 +0200

    Add user extended model.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import CASCADE, Model, OneToOneField, TextField
 
-# Create your models here.
+
+class Profile(Model):
+    user = OneToOneField(User, on_delete=CASCADE)
+    biography = TextField()
