906ad014e88ca3fdcde6228e523956fe73855292
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-06-14 16:27:48 +0200

    Add logout and password change.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from accounts.views import SubmittableLoginView
+from accounts.views import SubmittableLoginView, SubmittablePasswordChangeView
 
 app_name = 'accounts'
 urlpatterns = [
-    path('login/', SubmittableLoginView.as_view(), name='login')
+    path('login/', SubmittableLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path(
+        'password-change/', SubmittablePasswordChangeView.as_view(),
+        name='password_change'
+    )
 ]
