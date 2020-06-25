5f9fe91cd8564bd5b9504cbed90d41b806a65cc3
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-06-25 23:36:16 +0200

    Revert "Revert "Add movie_detail.css.""
    This reverts commit 6f3616e79155c214854b1007489eeb7454927052.

--- hollymovies/settings.py
+++ hollymovies/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static']
 
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
