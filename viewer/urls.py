2b63a866ea954589c2ba7f81ea1a32f8f2b8959e
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-06-26 18:12:49 +0200

    Add rest configuration.

--- viewer/urls.py
+++ viewer/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
 from viewer.views import (
     MovieCreateView, MovieDeleteView, MovieDetailView, MovieListView,
     MovieUpdateView
 )
+
+router = DefaultRouter()
 
 app_name = 'viewer'
 urlpatterns = [
@@ -11,5 +14,6 @@ urlpatterns = [
     path('movie/detail/<pk>', MovieDetailView.as_view(), name='movie_detail'),
     path('movie/create', MovieCreateView.as_view(), name='movie_create'),
     path('movie/update/<pk>', MovieUpdateView.as_view(), name='movie_update'),
-    path('movie/delete/<pk>', MovieDeleteView.as_view(), name='movie_delete')
+    path('movie/delete/<pk>', MovieDeleteView.as_view(), name='movie_delete'),
+    path('api/', include(router.urls))
 ]
