8163875c49597896e320968206f71776a4e9563f
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-07-02 22:28:26 +0200

    Fix statics.

--- hollymovies/settings.py
+++ hollymovies/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static-prod'
 STATICFILES_DIRS = ['static']
 
 # Default primary key field type
