b21ee067655d3d9b54a9032315f11ccd473c0b7d
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-07-02 22:28:26 +0200

    Fix statics.

--- hollymovies/settings.py
+++ hollymovies/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static-prod'
 STATICFILES_DIRS = ['static']
 
 MEDIA_ROOT = 'media'
