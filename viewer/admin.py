91d6eff553b4989ead34b98a4f9bc8ed71878969
Piotr Tokarski <piotr.albert.tokarski@gmail.com>
2020-09-13 14:53:15 +0200

    Add custom admin panel.

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,3 +1,44 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+
+class MovieAdmin(ModelAdmin):
+
+    @staticmethod
+    def released_year(obj):
+        return obj.released.year
+
+    @staticmethod
+    def cleanup_description(modeladmin, request, queryset):
+        queryset.update(description=None)
+
+    ordering = ['id']
+    list_display = ['id', 'title', 'genre', 'released_year']
+    list_display_links = ['id', 'title']
+    list_per_page = 20
+    list_filter = ['genre']
+    search_fields = ['title']
+    actions = ['cleanup_description']
+
+    fieldsets = [
+        (None, {'fields': ['title', 'created']}),
+        (
+            'External Information',
+            {
+                'fields': ['genre', 'released'],
+                'description': (
+                    'These fields are going to be filled with data parsed '
+                    'from external databases.'
+                )
+            }
+        ),
+        (
+            'User Information',
+            {
+                'fields': ['rating', 'description'],
+                'description': (
+                    'These fields are intended to be filled in by our users.'
+                )
+            }
+        )
+    ]
+    readonly_fields = ['created']
